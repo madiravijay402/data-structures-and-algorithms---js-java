43cc4144817f2e2650b1e86a99bfd59cddb0ce6c
madiravijay402 <vijaymadira681@gmail.com>
2025-12-31 20:40:23 +0530

    k

---
+++ day1 js/tree.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+class Main{
+    public static void main(string[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("enter the value :")
+        if(a>0){
+            System.out.println("positive");
+        }else{
+            System.out.println("negetive");
+        }
+    }
+}
