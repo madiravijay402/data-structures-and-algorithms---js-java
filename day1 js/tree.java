30753bcd0b42f07b9e2d806195cfae17df70ea37
madiravijay402 <vijaymadira681@gmail.com>
2026-01-30 22:07:05 +0530

    update

--- day1 js/tree.java
+++ day1 js/tree.java
@@ -1,9 +1,8 @@
-import java.util.scanner;
+import java.util.Scanner;
 class Main{
-    public static void main(string[] args){
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        System.out.println("enter the value :")
         if(a>0){
             System.out.println("positive");
         }else{
