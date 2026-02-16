485055bb091cdd918b740affa63aac2d1d2acca4
madiravijay402 <vijaymadira681@gmail.com>
2026-02-16 20:35:52 +0530

    update

--- day3/thread.java
+++ day3/thread.java
@@ -8,18 +8,20 @@ class A extends Thread{
 
 class B extends Thread{
     public void run(){
-        for(int i=5;i>=1;i++){
+        for(int i=5;i>=1;i--){
             System.out.println("B :"+i);
         }
     }
 }
 
 class thread{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         A t1=new A();
         B t2=new B();
         t1.start();
+        t1.join();
         t2.start();
+
 
     }
 }
