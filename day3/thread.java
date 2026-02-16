decdc6c7e0109cb9cd333b25f0585cec09981421
madiravijay402 <vijaymadira681@gmail.com>
2026-02-16 12:06:40 +0530

    update

---
+++ day3/thread.java
@@ -0,0 +1,25 @@
+class A extends Thread{
+    public void run(){
+       for(int i=1;i<=5;i++){
+        System.err.println("A :"+i);
+       }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for(int i=5;i>=1;i++){
+            System.out.println("B :"+i);
+        }
+    }
+}
+
+class thread{
+    public static void main(String[] args) {
+        A t1=new A();
+        B t2=new B();
+        t1.start();
+        t2.start();
+
+    }
+}
